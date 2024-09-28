30160ff03d73e41df25e08045044ccb7b7b42cd3
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-09-28 14:19:53 +0000

    commit

--- Query_]3.py
+++ Query_]3.py
@@ -19,4 +19,38 @@ while True:
         aluno = db["Hist_Aluno"]
         item = aluno.find()
         tab = pd.DataFrame(item)
-        print(tab.info())
+        tab["id_aluno"] = tab["id_aluno"].astype("string")
+        tab["codigo"] = tab["codigo"].astype("string")
+        disc = db["Disciplina"]
+        item2 = disc.find()
+        other = pd.DataFrame(item2)
+        other["_id"] = other["_id"].astype("string")
+        hist = tab.join(other.set_index('_id'), on='codigo', how='left', lsuffix='_hist', rsuffix='_disciplina')
+        print(hist[hist.id_aluno == '66ecab73360fa8661b05ddc8'].filter(items=['id_aluno', 'semestre_hist', 'ano', 'nota', 'nome', 'codigo_disciplina']))
+    if(op == 2):
+        prof = db['Hist_Professor']
+        item = prof.find()
+        tab = pd.DataFrame(item)
+        tab["id_prof"] = tab["id_prof"].astype("string")
+        tab["codigo"] = tab["codigo"].astype("string")
+        disc = db["Disciplina"]
+        item2 = disc.find()
+        other = pd.DataFrame(item2)
+        other["_id"] = other["_id"].astype("string")
+        hist = tab.join(other.set_index('_id'), on='codigo', how='left', lsuffix='_hist', rsuffix='_disciplina')
+        print(hist[hist.id_prof == '66ecae55360fa8661b083fe9'].filter(items=['id_prof', 'semestre_hist', 'ano', 'nome', 'codigo_disciplina']))
+    if(op == 3):
+        aluno = db['Aluno']
+        item = aluno.find()
+        tab = pd.DataFrame(item)
+        print(tab[tab.formado == "true"].filter(items=['_id', 'nome', 'ciclo']))
+    if(op == 4):
+        dep = db['Departamento']
+        item = dep.find()
+        tab = pd.DataFrame(item)
+        tab["id_prof"] = tab['id_prof'].astype("string")
+        prof = db['Professor']
+        item2 = prof.find()
+        other = pd.DataFrame(item2)
+        d = tab.join(other.set_index('_id'), on = 'id_prof', how = 'left', lsuffix="_dep", rsuffix="_prof")
+        print(d)
